5c8f329bbd0b83d117f7e1ea75551eca0aea438c
Krasimir Kuzmanov <krasimir.kuzmanov@gmail.com>
2016-08-21 17:48:54 +0300

    Landmarks Blog
    Uploading latest version of Landmarks Blog

---
+++ LandmarksBlog/Models/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LandmarksBlog.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            this.Date = DateTime.Now.Date;
+            this.Comments = new HashSet<Comment>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
